3f1e1b544cd9f86869a4a718f574e996c4f291c8
jeans <jw_github@headissue.com>
2013-12-23 20:26:55 +0100

    Release notes 0.19 started Reorganize around Clock*Cache and create new RandomCache implmentations Bugfix bulk code prefech code and experimental APIs for bulk ops

---
+++ core/src/main/java/org/cache2k/impl/RandomCache.java
@@ -0,0 +1,83 @@
+package org.cache2k.impl;
+
+/*
+ * #%L
+ * cache2k core package
+ * %%
+ * Copyright (C) 2000 - 2013 headissue GmbH, Munich
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the 
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public 
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+/**
+ * Random eviction without usage counters.
+ *
+ * @author Jens Wilke; created: 2013-12-22
+ */
+public class RandomCache<K, T> extends LockFreeCache<BaseCache.Entry, K, T> {
+
+  int evictionIndex = 0;
+
+  @Override
+  protected void recordHit(Entry entry) { }
+
+  @Override
+  protected void insertIntoReplcamentList(Entry entry) {
+    entry.next = entry;
+  }
+
+  @Override
+  protected Entry newEntry() {
+    return new Entry();
+  }
+
+  /**
+   * Start at arbitrary hash slot and evict the next best entry.
+   */
+  @Override
+  protected void evictEntry() {
+    Entry[] h0 = mainHash;
+    Entry[] h1 = refreshHash;
+    int idx = evictionIndex % (h0.length + h1.length);
+    if (idx >= h0.length) {
+      idx -= h0.length;
+      Entry[] t = h0;
+      h0 = h1;
+      h1 = t;
+    }
+    while (h0[idx] == null) {
+      idx++;
+      if (idx >= h0.length) {
+        Entry[] t = h0;
+        h0 = h1;
+        h1 = t;
+        idx = 0;
+      }
+    }
+    evictionIndex += h0[idx].hashCode;
+    if (evictionIndex < 0) {
+      evictionIndex = -evictionIndex;
+    }
+    h0[idx].removedFromList();
+    removeEntryFromCache(h0[idx]);
+  }
+
+  @Override
+  public long getHitCnt() {
+    return 0;
+  }
+
+}
