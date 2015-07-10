97a9e05aff0fa4c34e64fe0a383d47a90bdecf6c
Jens Wilke <jw_github@headissue.com>
2015-07-09 18:10:17 -0700

    propagate previous nextRefreshTime to have a proper state when calling the expiry calculator

---
+++ api/src/main/java/org/cache2k/CacheWriter.java
@@ -0,0 +1,41 @@
+package org.cache2k;
+
+/*
+ * #%L
+ * cache2k API only package
+ * %%
+ * Copyright (C) 2000 - 2015 headissue GmbH, Munich
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the 
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public 
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+/**
+ * @author Jens Wilke; created: 2015-04-24
+ */
+public interface CacheWriter<K, T> {
+
+  public void write(CacheEntry<K, T> e) throws Exception;
+
+  /**
+   * Called when a mapping is removed from the cache. An expiry does not trigger a call
+   * to this method.
+   *
+   * @param key
+   * @throws Exception if an exception occurs, the cache update will not occur
+   */
+  public void delete(Object key) throws Exception;
+
+}
