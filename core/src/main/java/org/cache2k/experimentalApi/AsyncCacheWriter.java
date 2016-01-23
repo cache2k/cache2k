4ece092836238959a2785e9bc3764650a2d19b42
Jens Wilke <jw_github@headissue.com>
2016-01-23 15:53:11 +0100

    experimental api for async loader and writer

---
+++ core/src/main/java/org/cache2k/experimentalApi/AsyncCacheWriter.java
@@ -0,0 +1,46 @@
+package org.cache2k.experimentalApi;
+
+/*
+ * #%L
+ * cache2k core package
+ * %%
+ * Copyright (C) 2000 - 2016 headissue GmbH, Munich
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the 
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public 
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+import java.util.EventListener;
+import java.util.concurrent.Executor;
+
+/**
+ * @author Jens Wilke
+ */
+public interface AsyncCacheWriter<K,V> {
+
+  void write(K key, V value, CompletedListener callback, Executor ex);
+
+  void remove(K key, CompletedListener callback, Executor ex);
+
+  /**
+   * @author Jens Wilke
+   */
+  interface CompletedListener extends EventListener {
+
+    void writeComplete();
+    void writeException(Throwable ex);
+
+  }
+}
