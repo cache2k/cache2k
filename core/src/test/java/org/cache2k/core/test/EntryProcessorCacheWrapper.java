994b063b97816441bebd68e7b34661e66b88b876
Jens Wilke <jw_github@headissue.com>
2016-01-28 11:26:32 +0100

    add tests, missed to add them...

---
+++ core/src/test/java/org/cache2k/core/test/EntryProcessorCacheWrapper.java
@@ -0,0 +1,114 @@
+package org.cache2k.core.test;
+
+/*
+ * #%L
+ * cache2k core package
+ * %%
+ * Copyright (C) 2000 - 2016 headissue GmbH, Munich
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the 
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public 
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+import org.cache2k.Cache;
+import org.cache2k.CacheEntryProcessor;
+import org.cache2k.MutableCacheEntry;
+import org.cache2k.extra.CacheWrapper;
+
+/**
+ * Override operations mutating or querying the cache by using the entry processor.
+ * This way we have a large test base for the entry processor, and we can verify that
+ * every possible cache operation can be expressed via an entry processor invocation.
+ * This class keeps the basic operations like peek, get, contains and put, since these
+ * will be used by the tests to set initial cache state and assert the correct behavior.
+ */
+public class EntryProcessorCacheWrapper<K, V> extends CacheWrapper<K, V> {
+
+  public EntryProcessorCacheWrapper(Cache<K, V> cache) {
+    super(cache);
+  }
+
+  /**
+   * Not replaces by entry processor invokation.
+   */
+  @Override
+  public V get(K key) {
+    return super.get(key);
+  }
+
+  /**
+   * Not replaces by entry processor invokation.
+   */
+  @Override
+  public V peek(K key) {
+    return super.peek(key);
+  }
+
+  /**
+   * Not replaces by entry processor invokation.
+   */
+  @Override
+  public boolean contains(K key) {
+    return super.contains(key);
+  }
+
+  /**
+   * Not replaces by entry processor invokation.
+   */
+  @Override
+  public void put(K key, V value) {
+    super.put(key, value);
+  }
+
+  @Override
+  public boolean replace(final K key, final V _newValue) {
+    CacheEntryProcessor<K, V, Boolean> p = new CacheEntryProcessor<K, V, Boolean>() {
+      @Override
+      public Boolean process(MutableCacheEntry<K, V> entry, Object... arguments) throws Exception {
+        if (!entry.exists()) {
+          return false;
+        }
+        entry.setValue(_newValue);
+        return true;
+      }
+    };
+    return invoke(key, p);
+  }
+
+  @Override
+  public boolean replace(final K key, final V _oldValue, final V _newValue) {
+    CacheEntryProcessor<K, V, Boolean> p = new CacheEntryProcessor<K, V, Boolean>() {
+      @Override
+      public Boolean process(MutableCacheEntry<K, V> entry, Object... arguments) throws Exception {
+        if (!entry.exists()) {
+          return false;
+        }
+        if (_oldValue == null) {
+          if (null != entry.getValue()) {
+            return false;
+          }
+        } else {
+          if (!_oldValue.equals(entry.getValue())) {
+            return false;
+          }
+        }
+        entry.setValue(_newValue);
+        return true;
+      }
+    };
+    return invoke(key, p);
+  }
+
+}
---
+++ core/src/test/java/org/cache2k/core/test/Statistics.java
@@ -0,0 +1,121 @@
+package org.cache2k.core.test;
+
+/*
+ * #%L
+ * cache2k core package
+ * %%
+ * Copyright (C) 2000 - 2016 headissue GmbH, Munich
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the 
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public 
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+import org.cache2k.Cache;
+import org.cache2k.impl.InternalCache;
+import org.cache2k.impl.InternalCacheInfo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Abstract how to access counter values from the cache and a fluent API
+ * to do assertions on the expected values for crispy tests.
+ *
+ * <p>The basic idea is not to do assertions on the absolute counter values
+ * but on the delta values. The exposed interface for the tests is the
+ * ability to retrieve the counter value, assert on a value and to reset
+ * one or all values.
+ *
+ * @author Jens Wilke
+ */
+public class Statistics {
+
+  InternalCacheInfo info;
+  List<Counter> counters = new ArrayList<Counter>();
+
+  public final Counter readCount = new Counter("read") {
+    @Override
+    protected long getCounterValue(final InternalCacheInfo inf) {
+      return inf.getReadUsageCnt();
+    }
+  };
+
+  public final Counter removeCount = new Counter("remove") {
+    @Override
+    protected long getCounterValue(final InternalCacheInfo inf) {
+      return inf.getRemovedCnt();
+    }
+  };
+
+  /** Reset all counters */
+  public void reset() {
+    for (Counter c : counters) {
+      c.reset();
+    }
+  }
+
+  /** Get the latest counter values from the cache. */
+  public void sample(Cache c) {
+    info = ((InternalCache) c).getLatestInfo();
+  }
+
+  /** Checks that all counter deltas are zero */
+  public void expectAllZero() {
+    for (Counter c : counters) {
+      c.expect(0);
+    }
+
+  }
+
+  public abstract class Counter {
+
+    long baseValue;
+    String name;
+
+    public Counter(final String _name) {
+      name = _name;
+      counters.add(this);
+    }
+
+    protected abstract long getCounterValue(InternalCacheInfo inf);
+
+    public Statistics reset() {
+      baseValue += get();
+      return Statistics.this;
+    }
+
+    /**
+     * The counter value, actually the delta value since the last reset.
+     */
+    public long get() {
+      return getCounterValue(info) - baseValue;
+    }
+
+    /**
+     * Asserts on an expected counter value and resets it. By resetting
+     * this operation can be followed by {@link #expectAllZero()} to check
+     * that all other counters are 0.
+     */
+    public Statistics expect(long v) {
+      assertEquals(name + " counter", v, get());
+      reset();
+      return Statistics.this;
+    }
+
+  }
+
+}
