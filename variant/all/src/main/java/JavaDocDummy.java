e3b0b9296a3e25f53de787be7e414822c1312d04
Jens Wilke <jw_github@headissue.com>
2016-03-29 12:52:39 +0200

    need dummy class to produce emtpy java doc and sources. remove wrong usage of jar plugin. fixes: https://github.com/cache2k/cache2k/issues/35

---
+++ variant/all/src/main/java/JavaDocDummy.java
@@ -0,0 +1,30 @@
+/*
+ * #%L
+ * cache2k-all complete bundle
+ * %%
+ * Copyright (C) 2000 - 2016 headissue GmbH, Munich
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the 
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public 
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+/**
+ * This is a bundle artifact which contains no JavaDoc and source.
+ * For accessing the API source and documentation add a dependency to cache2k-api
+ * to your compile scope.
+ *
+ * @author Jens Wilke
+ */
+public class JavaDocDummy {
+}
