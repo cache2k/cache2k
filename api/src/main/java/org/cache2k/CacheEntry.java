743be338e354f65656840708daef17cd02a34f40
Jens Wilke <jw_github@headissue.com>
2016-03-29 12:49:15 +0200

    doc

--- api/src/main/java/org/cache2k/CacheEntry.java
+++ api/src/main/java/org/cache2k/CacheEntry.java
@@ -25,28 +25,44 @@ package org.cache2k;
 /**
  * Object representing a cache entry. With the cache entry, it can be
  * checked whether a mapping in the cache is present, even if the cache
- * holds null or an exception.
+ * holds null or an exception. Entries can be retrieved by
+ * {@link Cache#peekEntry(Object)} or {@link Cache#getEntry(Object)} or
+ * via {@link Cache#iterator()}.
  *
  * <p>After retrieved, the entry instance does not change its values, even
  * if the value for its key is updated in the cache.
  *
  * <p>Design note: The cache is generally also aware of the time the
- * object will be refreshed next or when it expired. This is not exposed
+ * object will be refreshed next or when it will expire. This is not exposed
  * to applications by intention.
  *
  * @author Jens Wilke; created: 2014-03-18
+ * @see Cache#peekEntry(Object)
+ * @see Cache#getEntry(Object)
+ * @see Cache#iterator()
  */
 public interface CacheEntry<K, V> {
 
+  /**
+   * Key associated with this entry.
+   */
   K getKey();
 
+  /**
+   * Value of the entry. The value may be null if nulls are allowed, or,
+   * if an exception was thrown by the loader.
+   */
   V getValue();
 
+  /**
+   * If not null a exception happened when the value was loaded and
+   * the exception cannot be suppressed.
+   */
   Throwable getException();
 
   /**
-   * Time in millis the entry was last updated either by a fetch via the CacheSource
-   * or by a put. If the entry was never fetched 0 is returned.
+   * Time in millis the entry was last updated either by loading
+   * or by a put.
    */
   long getLastModification();
 
