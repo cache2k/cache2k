0630f994d09e3ec0973537d3211f1914214f1f42
Jens Wilke <jw_github@headissue.com>
2016-05-05 15:59:58 +0200

    fix missing expiry information if exception is cached eternally

--- core/src/main/java/org/cache2k/core/EntryAction.java
+++ core/src/main/java/org/cache2k/core/EntryAction.java
@@ -519,7 +519,7 @@ public abstract class EntryAction<K, V, R> implements StorageCallback, AsyncCach
         }
         if (expiry < 0) {
           ew.until = -expiry;
-        } else if (expiry >= Entry.EXPIRY_TIME_MIN && expiry != Long.MAX_VALUE) {
+        } else if (expiry >= Entry.EXPIRY_TIME_MIN) {
           ew.until = expiry;
         }
       } else {
