f5d280e31881261013045e8d012580799aff88ff
Jens Wilke <jw_github@headissue.com>
2017-01-02 15:14:15 +0100

    extend copyright to next year

--- cache2k-core/src/main/java/org/cache2k/core/experimentalApi/AsyncCacheLoader.java
+++ cache2k-core/src/main/java/org/cache2k/core/experimentalApi/AsyncCacheLoader.java
@@ -4,7 +4,7 @@ package org.cache2k.core.experimentalApi;
  * #%L
  * cache2k core
  * %%
- * Copyright (C) 2000 - 2016 headissue GmbH, Munich
+ * Copyright (C) 2000 - 2017 headissue GmbH, Munich
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- cache2k-xml-configuration/src/main/java/org/cache2k/xmlConfiguration/SourceLocation.java
+++ cache2k-xml-configuration/src/main/java/org/cache2k/xmlConfiguration/SourceLocation.java
@@ -4,7 +4,7 @@ package org.cache2k.xmlConfiguration;
  * #%L
  * cache2k XML configuration
  * %%
- * Copyright (C) 2000 - 2016 headissue GmbH, Munich
+ * Copyright (C) 2000 - 2017 headissue GmbH, Munich
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
