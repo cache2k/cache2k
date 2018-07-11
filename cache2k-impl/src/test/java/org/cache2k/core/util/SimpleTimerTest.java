bd4bc4dc2300d07f5a36a847f638fe328c739bf1
Jens Wilke <jw_github@headissue.com>
2018-07-11 08:20:01 +0200

    fix flaky test

--- cache2k-impl/src/test/java/org/cache2k/core/util/SimpleTimerTest.java
+++ cache2k-impl/src/test/java/org/cache2k/core/util/SimpleTimerTest.java
@@ -37,6 +37,7 @@ public class SimpleTimerTest {
   @Test
   public void purge() {
     long _START_TIME = 100;
+    long _OFFSET = 1;
     int _SIZE = 123;
     SimpleTimer t =
       new SimpleTimer(
@@ -45,13 +46,13 @@ public class SimpleTimerTest {
     MyTask[] arr = new MyTask[_SIZE];
     for (int i = 0; i < _SIZE; i++) {
       arr[i] = new MyTask();
-      t.schedule(arr[i], _START_TIME + i);
+      t.schedule(arr[i], _START_TIME + i + _OFFSET);
       if (i%3 == 0) {
         arr[i].cancel();
       }
     }
     t.purge();
-    t.timeReachedEvent(_START_TIME + _SIZE);
+    t.timeReachedEvent(_START_TIME + _SIZE + _OFFSET);
     int count = 0;
     for (int i = 0; i < _SIZE; i++) {
       if (arr[i].executed) {
