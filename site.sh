d9118312c2429294d53e95246e23fe9db67605dc
Jens Wilke <jw_github@headissue.com>
2018-07-21 11:49:58 +0200

    doc touch up, add JCache java doc to the website

--- site.sh
+++ site.sh
@@ -9,7 +9,7 @@ export MAVEN_OPTS="-XX:+TieredCompilation -XX:TieredStopAtLevel=1"
 
 DEST=target/site;
 
-mvn -Psite -Dmaven.test.skip=true -T 1.5C clean compile site
+mvn -Dmaven.test.skip=true -T 1.5C clean compile site
 
 # install stable version of 1.0 documentation
 VERSION=1.0.2.Final
@@ -17,10 +17,18 @@ DOC=$DEST/docs/1.0
 mkdir -p $DOC/apidocs
 cp documentation/generated-user-guide-archive-1.0/* $DOC
 for I in cache2k-api cache2k-jmx-api cache2k-jcache-api; do
-  mvn org.apache.maven.plugins:maven-dependency-plugin:3.1.0:get -Dartifact=org.cache2k:$I:$VERSION:jar:javadoc
+  fn=~/.m2/repository/org/cache2k/$I/$VERSION/$I-$VERSION-javadoc.jar;
+  test -f $fn || mvn org.apache.maven.plugins:maven-dependency-plugin:3.1.0:get -Dartifact=org.cache2k:$I:$VERSION:jar:javadoc
   mkdir -p $DOC/apidocs/$I/
-  ( cd  $DOC/apidocs/$I/; jar xf ~/.m2/repository/org/cache2k/$I/$VERSION/$I-$VERSION-javadoc.jar )
+  ( cd  $DOC/apidocs/$I/; jar xf $fn )
 done
+
+DOC=$DEST/docs/jcache/1.1.0/cache-api/apidocs
+# JSR107 JCache
+fn=~/.m2/repository/javax/cache/cache-api/1.1.0/cache-api-1.1.0-javadoc.jar;
+test -f $fn || mvn org.apache.maven.plugins:maven-dependency-plugin:3.1.0:get -Dartifact=javax.cache:cache-api:1.1.0:jar:javadoc
+mkdir -p $DOC
+( cd  $DOC; jar xf $fn )
 
 # 1.0 is latest
 # cp -a $DEST/docs/1.0 $DEST/docs/latest
