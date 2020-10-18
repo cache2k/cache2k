dd30d30fd24620ccc410f1b9a0a36cebae0cee1a
Jens Wilke <jw_github@headissue.com>
2020-10-19 06:38:47 +1000

    check for slf4j pick, add maven module test-init-java11 to test with JigSaw modules and module-info.java

---
+++ test-init-java11/src/test/java/test/InitTest.java
@@ -0,0 +1,52 @@
+package test;
+
+/*
+ * #%L
+ * cache2k initialization tests Java 11
+ * %%
+ * Copyright (C) 2000 - 2020 headissue GmbH, Munich
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import org.assertj.core.api.Assertions;
+import org.cache2k.Cache;
+import org.cache2k.Cache2kBuilder;
+import org.cache2k.CacheManager;
+import static org.junit.Assert.*;
+
+import org.cache2k.configuration.Cache2kConfiguration;
+import org.cache2k.core.log.Log;
+import org.junit.Test;
+
+/**
+ * @author Jens Wilke
+ */
+public class InitTest {
+
+  @Test
+  public void config() {
+    Cache2kBuilder b = Cache2kBuilder.forUnknownTypes();
+    b.name("testCache");
+    b.build();
+    assertEquals(1802, b.toConfiguration().getEntryCapacity());
+  }
+
+  @Test
+  public void slf4jInUse() {
+    Log l = Log.getLog(InitTest.class);
+    Assertions.assertThat(l).isInstanceOf(Log.Slf4jLogger.class);
+  }
+
+}
