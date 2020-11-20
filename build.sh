d050a58ba6ec2b2c1a281ff0b2de15018d91a012
Jens Wilke <jw_github@headissue.com>
2020-11-20 14:36:16 +1000

    thread dump after 5 mins don't use signal since output is consumed by surefire

--- build.sh
+++ build.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 
-trap 'kill $(jobs -p) 2>/dev/null' EXIT
+trap 'kill $(jobs -rp) 2>/dev/null' EXIT
 set -ex
+
+printThreadDumps() {
+  jps -lv | while read I; do
+    pid=`echo "$I" | awk '{ print $1; }'`
+    echo
+    echo "Thread dump process: $I";
+    jstack -l $pid || true;
+  done
+}
 
 java -version
 mvn clean install -DskipTests=true -Dmaven.javadoc.skip=true -B -V
-( sleep $(( 60 * 9 )); kill -3 `jps -q` ) &
+( sleep $(( 60 * 5 ));
+  printThreadDumps;
+  sleep 10;
+  printThreadDumps;
+  sleep 10;
+  printThreadDumps;
+  echo "TIMEOUT"
+  exit 1;
+) &
 threadDumpPid=$!
 mvn test -B &
 testPid=$!
