04b982857d3650a034d10e336634bbaf33f23893
Jens Wilke <jw_github@headissue.com>
2022-01-10 06:04:22 +0100

    add manager test

---
+++ cache2k-testsuite/src/main/java/org/cache2k/testsuite/manager/CacheManagerTest.java
@@ -0,0 +1,43 @@
+package org.cache2k.testsuite.manager;
+
+/*-
+ * #%L
+ * cache2k testsuite on public API
+ * %%
+ * Copyright (C) 2000 - 2022 headissue GmbH, Munich
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import org.cache2k.CacheManager;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+/**
+ * @author Jens Wilke
+ */
+public class CacheManagerTest {
+
+  /**
+   * Defined behavior of null cache name: throw NPE
+   */
+  @Test
+  public void getInstance_NullName() {
+    assertThatCode(() -> {
+      CacheManager.getInstance((String) null);
+    }).isInstanceOf(NullPointerException.class);
+  }
+
+}
